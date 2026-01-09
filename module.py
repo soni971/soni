2676b77c2c37895087857b824964c4748cda49cf
soni971 <sonipathik36@gmail.com>
2026-01-09 14:51:39 +0530

    Add files via upload

---
+++ module.py
@@ -0,0 +1,67 @@
+# Inbuilt function in modules
+
+# 1-In_built function MATH:
+
+# import math
+# print(math.factorial(5))
+# print(math.sqrt(9))
+# print(math.pow(2,4))
+# print(math.pi)
+
+
+# import datetime
+# # Date time NOW
+# now=datetime.datetime.now()
+# print(now)
+
+# # Today Date
+# today=datetime.date.today()
+# print(today)
+
+
+# Future Date
+
+# future=Today=datetime.timedelta(days=10)
+# print("10 days later:",future)
+
+# Python Version System
+
+# import sys
+# print(sys.version)
+# print(sys.executable)
+# print(sys.platform)
+
+# import random
+# print(random.randint(1,10))
+# print(random.random())
+# print(random.choice(["AIPA","CSA","COPA"]))
+# trade=["AI","COPA","EM","DM"]
+# random.shuffle(trade)
+# print(trade)
+
+
+
+# import os
+# print(os.getcwd())
+# print(os.listdir())
+# os.mkdir("Shinchan")                        # NEW FLODER CREATE
+# os.rmdir("Shinchan")                      #  REMOVE FLODER
+
+
+
+
+
+
+
+
+
+import time
+print(time.ctime())
+time.sleep(5)
+print("soni 5sec")
+
+
+
+
+
+
